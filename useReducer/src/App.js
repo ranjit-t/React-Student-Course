a71def90ef772111496558e8b290a02f36ff7395
Ranjit <amailtoranjith@gmail.com>
2023-02-28 11:24:16 +0100

    useReducer

---
+++ useReducer/src/App.js
@@ -0,0 +1,98 @@
+import "./App.css";
+import { useReducer } from "react";
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "NewUserInput":
+//       return { ...state, Input: action.payload };
+
+//     default:
+//       return "something is wrong";
+//   }
+// };
+
+// function App() {
+//   const [state, dispatch] = useReducer(reducer, { Input: "" });
+//   return (
+//     <div className="App">
+//       <h2>useReducer</h2>
+//       <label>
+//         Type :{" "}
+//         <input
+//           type="text"
+//           placeholder="enter"
+//           value={state.Input}
+//           onChange={(e) => {
+//             dispatch({ type: "NewUserInput", payload: e.target.value });
+//           }}
+//         />
+//       </label>
+//       <p>{state.Input}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "NewInput":
+      return { ...state, Input: action.payload };
+    case "decrease":
+      return { ...state, count: state.count - 1 };
+    case "increase":
+      return { ...state, count: state.count + 1 };
+    case "reset":
+      return { ...state, count: 0 };
+    default:
+      console.log("something is wrong");
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { Input: "", count: 0 });
+  return (
+    <div className="App">
+      <h2>useReducer Demo</h2>
+      <input
+        type="text"
+        placeholder="enter and see the magic"
+        onChange={(e) => {
+          dispatch({ type: "NewInput", payload: e.target.value });
+        }}
+      />
+      <p>{state.Input.length === 0 ? <span>Welcome</span> : state.Input}</p>
+      <div>
+        <p>{state.count}</p>
+        <p>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch({ type: "decrease" });
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch({ type: "reset" });
+            }}
+          >
+            Reset
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch({ type: "increase" });
+            }}
+          >
+            +
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
